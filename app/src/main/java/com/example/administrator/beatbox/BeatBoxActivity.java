ef66398eb347c57f82d62c2bd41f6c57320118ad
andrew.gong <guantbb@126.com>
2019-03-26 21:49:54 +0800

    复用SingleFragmentActivity.java 及其相关布局xml

--- app/src/main/java/com/example/administrator/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/example/administrator/beatbox/BeatBoxActivity.java
@@ -1,13 +1,19 @@
 package com.example.administrator.beatbox;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentacActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
     }
 }
