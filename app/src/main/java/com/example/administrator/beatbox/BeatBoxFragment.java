a20a217d98e67c8fed5cfbc9617161f95529e1fd
andrew.gong <guantbb@126.com>
2019-03-26 22:36:03 +0800

    获取绑定视图中的对象————实例化

--- app/src/main/java/com/example/administrator/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/example/administrator/beatbox/BeatBoxFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,8 +23,19 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        /**
+         * 1 前一个Criminal应用使用[inflater]来加载指定layout xml中的所有视图组件
+         * 2 然后使用view的find id 接口方法来找到RecyclerView
+         * 但是
+         * 这里的BeatBox应用中使用了数据绑定的“高级”方式来实例化RecycleView对象
+         */
         FragmentBeatBoxBinding binding =   DataBindingUtil.inflate(inflater,R.layout.fragment_beat_box, container,false);
+        binding.recycleView.setLayoutManager(new GridLayoutManager(getActivity(),3));
 
+        /**
+         * getRoot()返回全部的FragmentBeatBoxBinding对象中绑定的Root View ,即全部的视图组件容器
+         * binding.recycleView 指向上述容器中的特定视图对象——recycleView
+         */
         return binding.getRoot();
     }
 }
