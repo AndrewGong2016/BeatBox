75ce420ffac9a532af7a2399d0bf9aee34fda00a
andrew.gong <guantbb@126.com>
2019-03-26 23:20:16 +0800

    add sounds to app/assets/sample_sounds/,but found 0 files when running the app . you should fix it later !

--- app/src/main/java/com/example/administrator/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/example/administrator/beatbox/BeatBoxFragment.java
@@ -6,17 +6,28 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.administrator.beatbox.databinding.FragmentBeatBoxBinding;
+import com.example.administrator.beatbox.databinding.ListItemSoundBinding;
 
 public class BeatBoxFragment extends Fragment {
 
+    private BeatBox mBeatBox;
 
     public static BeatBoxFragment newInstance(){
         return new BeatBoxFragment();
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Nullable
@@ -31,6 +42,7 @@ public class BeatBoxFragment extends Fragment {
          */
         FragmentBeatBoxBinding binding =   DataBindingUtil.inflate(inflater,R.layout.fragment_beat_box, container,false);
         binding.recycleView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        binding.recycleView.setAdapter(new SoundAdapter());
 
         /**
          * getRoot()返回全部的FragmentBeatBoxBinding对象中绑定的Root View ,即全部的视图组件容器
@@ -38,5 +50,44 @@ public class BeatBoxFragment extends Fragment {
          */
         return binding.getRoot();
     }
+
+
+    private class SoundHolder extends RecyclerView.ViewHolder{
+        ListItemSoundBinding mBinding;
+        private SoundHolder(ListItemSoundBinding binding){
+            /**
+             * 把视图实例化的工作全部交给bingding对象
+             */
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+
+        @NonNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            /**
+             * 同上，使用DataBindingUtil 生成的Binding对象来构建视图所需的Holder
+             */
+            ListItemSoundBinding soundBinding = DataBindingUtil.inflate(inflater,R.layout.list_item_sound,viewGroup,false);
+            return new SoundHolder(soundBinding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder soundHolder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+
 }
 
